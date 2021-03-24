b0dfc9f536d5b36ae0e5590bf0e18fb5f5e0ad48
Hubert Rzeminski <hubertrzeminski16@gmail.com>
2021-03-24 06:42:22 +0000

    Initial Commit

---
+++ MeditationApp/MeditationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeditationAppApp.swift
+//  MeditationApp
+//
+//  Created by Hubert Rzeminski on 24/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct MeditationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
